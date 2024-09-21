64afd1006c17b14fe95287c515f0380d713b8f8e
Marcoslima016 <marcosvr.lima96@gmail.com>
2024-09-20 22:45:31 -0300

    refactor

--- lib/lib.exports.dart
+++ lib/lib.exports.dart
@@ -1,5 +1,4 @@
-export 'lib.exports.dart'; 
-export 'main.dart'; 
-export './app/app.exports.dart'; 
-export './core/core.exports.dart'; 
-export './shared/shared.exports.dart'; 
+export 'lib.exports.dart';
+export 'main.dart';
+export './app/app.exports.dart';
+export './shared/shared.exports.dart';
